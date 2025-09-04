cb3a0e9883049abf39e62f79cc6817573b2a32e6
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-09-05 07:25:27 +0900

    feat: AsImageを&[u8]に実装

--- crates/aviutl2/src/input/binding.rs
+++ crates/aviutl2/src/input/binding.rs
@@ -195,6 +195,12 @@ impl AsImage for Vec<u8> {
     }
 }
 
+impl AsImage for &[u8] {
+    fn as_image(&'_ self) -> Cow<'_, [u8]> {
+        Cow::Borrowed(self)
+    }
+}
+
 duplicate::duplicate! {
     [
         Name            Trait     method;
