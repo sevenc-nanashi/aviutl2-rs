29d0202767f5256486f897330e8e4daa288985bc
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-10-23 20:07:52 +0900

    chore: sdkを更新

--- crates/aviutl2-sys/src/lib.rs
+++ crates/aviutl2-sys/src/lib.rs
@@ -5,6 +5,7 @@
 //! - [`input2`][]：`input2.h`相当。
 //! - [`output2`][]：`output2.h`相当。
 //! - [`filter2`][]：`filter2.h`相当。
+//! - [`module2`][]：`module2.h`相当。
 //!
 //! - [`common`][]：共通定義。
 
@@ -14,3 +15,4 @@ pub mod common;
 pub mod filter2;
 pub mod input2;
 pub mod output2;
+pub mod module2;
