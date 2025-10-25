cf96aedee0e6f9c49e9441b960db1e32c11e4d88
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-10-25 18:33:15 +0900

    fix: サンプルのテストを修正

--- examples/username-module/src/lib.rs
+++ examples/username-module/src/lib.rs
@@ -2,21 +2,6 @@ use aviutl2::{AnyResult, module::ScriptModuleFunctions};
 
 struct UsernameModule;
 
-struct MyModule;
-#[aviutl2::module::functions]
-impl MyModule {
-    fn sum(a: i32, b: i32) -> i32 {
-        a + b
-    }
-
-    #[direct]
-    fn direct_sum(params: &aviutl2::module::ScriptModuleCallHandle) {
-        let a: i32 = params.get_param(0).unwrap_or(0);
-        let b: i32 = params.get_param(1).unwrap_or(0);
-        params.push_result(&(a + b));
-    }
-}
-
 impl aviutl2::module::ScriptModule for UsernameModule {
     fn new(_info: aviutl2::AviUtl2Info) -> AnyResult<Self> {
         Ok(UsernameModule)
