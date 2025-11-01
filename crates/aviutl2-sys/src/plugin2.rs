628f2146aff8df6db446c74a825689c05071a94c
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-11-01 17:41:11 +0900

    chore: コメントを揃える

--- crates/aviutl2-sys/src/plugin2.rs
+++ crates/aviutl2-sys/src/plugin2.rs
@@ -7,13 +7,13 @@ use std::os::raw::c_char;
 
 pub use windows_sys::Win32::Foundation::HWND;
 
-/// Cの`LPCSTR`相当（UTF-8 のヌル終端文字列）
 pub type LPCSTR = *const c_char;
 
-/// オブジェクトハンドル（不透明ポインタ）
+/// オブジェクトハンドル
 pub type OBJECT_HANDLE = *mut c_void;
 
 /// レイヤー・フレーム情報構造体
+/// フレーム番号、レイヤー番号が0からの番号になります ※UI表示と異なります
 #[repr(C)]
 pub struct OBJECT_LAYER_FRAME {
     /// レイヤー番号
@@ -24,7 +24,7 @@ pub struct OBJECT_LAYER_FRAME {
     pub end: i32,
 }
 
-/// 冗長（非推奨）オブジェクトフレーム情報構造体
+/// 冗長なので後で廃止します
 #[repr(C)]
 pub struct DEPRECATED_OBJECT_FRAME_INFO {
     pub start: i32,
@@ -32,6 +32,7 @@ pub struct DEPRECATED_OBJECT_FRAME_INFO {
 }
 
 /// 編集情報構造体
+/// フレーム番号、レイヤー番号が0からの番号になります ※UI表示と異なります
 #[repr(C)]
 pub struct EDIT_INFO {
     /// シーンの解像度
@@ -40,7 +41,7 @@ pub struct EDIT_INFO {
     pub height: i32,
     /// シーンのフレームレート
     pub rate: i32,
-    /// シーンのフレームレート（スケール）
+    /// シーンのフレームレート
     pub scale: i32,
     /// シーンのサンプリングレート
     pub sample_rate: i32,
@@ -54,35 +55,65 @@ pub struct EDIT_INFO {
     pub layer_max: i32,
 }
 
-/// 編集セクション構造体（プロジェクト編集用の機能群）
+/// 編集セクション構造体
+/// メニュー選択やプロジェクト編集のコールバック関数内で利用出来ます
+/// フレーム番号、レイヤー番号が0からの番号になります ※UI表示と異なります
 #[repr(C)]
 pub struct EDIT_SECTION {
     /// 編集情報
     pub info: *mut EDIT_INFO,
 
-    /// 指定位置にオブジェクトエイリアスを作成
+    /// 指定の位置にオブジェクトエイリアスを作成します
+    /// alias	: オブジェクトエイリアスデータ(UTF-8)へのポインタ
+    /// 			  オブジェクトエイリアスファイルと同じフォーマットになります
+    /// layer	: 作成するレイヤー番号
+    /// frame	: 作成するフレーム番号
+    /// length	: オブジェクトのフレーム数 ※エイリアスデータにフレーム情報が無い場合に利用します
+    /// 戻り値	: 作成したオブジェクトのハンドル (失敗した場合はnullptrを返却)
+    /// 			  既に存在するオブジェクトに重なったり、エイリアスデータが不正な場合に失敗します
     pub create_object_from_alias:
         unsafe extern "C" fn(alias: LPCSTR, layer: i32, frame: i32, length: i32) -> OBJECT_HANDLE,
 
-    /// 指定フレーム以降でオブジェクトを検索
+    /// 指定のフレーム番号以降にあるオブジェクトを検索します
+    /// layer	: 検索対象のレイヤー番号
+    /// frame	: 検索を開始するフレーム番号
+    /// 戻り値	: 検索したオブジェクトのハンドル (見つからない場合はnullptrを返却)
     pub find_object: unsafe extern "C" fn(layer: i32, frame: i32) -> OBJECT_HANDLE,
 
-    /// 冗長（非推奨）フレーム情報の取得
+    /// 冗長なので後で廃止します
     pub deprecated_get_object_frame_info:
         unsafe extern "C" fn(object: OBJECT_HANDLE) -> DEPRECATED_OBJECT_FRAME_INFO,
 
-    /// レイヤー・フレーム情報の取得
+    /// オブジェクトのレイヤー・フレーム情報を取得します
+    /// object	: オブジェクトのハンドル
+    /// 戻り値	: オブジェクトのレイヤー・フレーム情報
     pub get_object_layer_frame:
         unsafe extern "C" fn(object: OBJECT_HANDLE) -> OBJECT_LAYER_FRAME,
 
-    /// オブジェクトのエイリアスデータ取得（UTF-8）
+    /// オブジェクトのエイリアスデータを取得します
+    /// object	: オブジェクトのハンドル
+    /// 戻り値	: オブジェクトエイリアスデータ(UTF-8)へのポインタ (取得出来ない場合はnullptrを返却)
+    /// 			  オブジェクトエイリアスファイルと同じフォーマットになります
+    /// 			  ※次に文字列返却の関数を使うかコールバック処理の終了まで有効
     pub get_object_alias: unsafe extern "C" fn(object: OBJECT_HANDLE) -> LPCSTR,
 
-    /// 設定項目の値を文字列（UTF-8）で取得
+    /// オブジェクトの設定項目の値を文字列で取得します
+    /// object	: オブジェクトのハンドル
+    /// effect	: 対象のエフェクト名 (エイリアスファイルのeffect.nameの値)
+    /// item		: 対象の設定項目の名称 (エイリアスファイルのキーの名称)
+    /// 戻り値	: 取得した設定値(UTF8)へのポインタ (取得出来ない場合はnullptrを返却)
+    /// 			  エイリアスファイルの設定値と同じフォーマットになります
+    /// 			  ※次に文字列返却の関数を使うかコールバック処理の終了まで有効
     pub get_object_item_value:
         unsafe extern "C" fn(object: OBJECT_HANDLE, effect: LPCWSTR, item: LPCWSTR) -> LPCSTR,
 
-    /// 設定項目の値を文字列（UTF-8）で設定
+    /// オブジェクトの設定項目の値を文字列で設定します
+    /// object	: オブジェクトのハンドル
+    /// effect	: 対象のエフェクト名 (エイリアスファイルのeffect.nameの値)
+    /// item		: 対象の設定項目の名称 (エイリアスファイルのキーの名称)
+    /// value	: 設定値(UTF8)
+    /// 			  エイリアスファイルの設定値と同じフォーマットになります
+    /// 戻り値	: 設定出来た場合はtrue (対象が見つからない場合は失敗します)
     pub set_object_item_value: unsafe extern "C" fn(
         object: OBJECT_HANDLE,
         effect: LPCWSTR,
@@ -90,76 +121,116 @@ pub struct EDIT_SECTION {
         value: LPCSTR,
     ) -> bool,
 
-    /// オブジェクトを移動
+    /// オブジェクトを移動します
+    /// object	: オブジェクトのハンドル
+    /// layer	: 移動先のレイヤー番号
+    /// frame	: 移動先のフレーム番号
+    /// 戻り値	: 移動した場合はtrue (移動先にオブジェクトが存在する場合は失敗します)
     pub move_object: unsafe extern "C" fn(object: OBJECT_HANDLE, layer: i32, frame: i32) -> bool,
 
-    /// オブジェクトを削除
+    /// オブジェクトを削除します
+    /// object	: オブジェクトのハンドル
     pub delete_object: unsafe extern "C" fn(object: OBJECT_HANDLE),
 
-    /// 設定ウィンドウで選択中のオブジェクト取得
+    /// オブジェクト設定ウィンドウで選択されているオブジェクトのハンドルを取得します
+    /// 戻り値	: オブジェクトのハンドル (未選択の場合はnullptrを返却)
     pub get_focus_object: unsafe extern "C" fn() -> OBJECT_HANDLE,
 
-    /// 設定ウィンドウで選択するオブジェクトを設定
+    /// オブジェクト設定ウィンドウで選択するオブジェクトを設定します (コールバック処理の終了時に設定されます)
+    /// object	: オブジェクトのハンドル
     pub set_focus_object: unsafe extern "C" fn(object: OBJECT_HANDLE),
 }
 
 /// 編集ハンドル構造体
 #[repr(C)]
 pub struct EDIT_HANDLE {
-    /// プロジェクトデータの編集を行うコールバックを呼び出す
+    /// プロジェクトデータの編集をする為のコールバック関数(func_proc_edit)を呼び出します
+    /// 編集情報を排他制御する為にコールバック関数内で編集処理をする形になります
+    /// コールバック関数内で編集したオブジェクトは纏めてUndoに登録されます
+    /// コールバック関数はメインスレッドから呼ばれます
+    /// func_proc_edit	: 編集処理のコールバック関数
+    /// 戻り値			: trueなら成功
+    /// 					  編集が出来ない場合(出力中等)に失敗します
     pub call_edit_section:
         unsafe extern "C" fn(func_proc_edit: unsafe extern "C" fn(edit: *mut EDIT_SECTION)) -> bool,
 }
 
 /// プロジェクトファイル構造体
+/// プロジェクトファイルのロード、セーブ時のコールバック関数内で利用出来ます
+/// プロジェクトの保存データはプラグイン毎のデータ領域になります
 #[repr(C)]
 pub struct PROJECT_FILE {
-    /// 文字列(UTF-8)を取得
+    /// プロジェクトに保存されている文字列(UTF-8)を取得します
+    /// key		: キー名(UTF-8)
+    /// 戻り値	: 取得した文字列へのポインタ (未設定の場合はnullptr)
     pub get_param_string: unsafe extern "C" fn(key: LPCSTR) -> LPCSTR,
-    /// 文字列(UTF-8)を保存
+    /// プロジェクトに文字列(UTF-8)を保存します
+    /// key		: キー名(UTF-8)
+    /// value	: 保存する文字列(UTF-8)
     pub set_param_string: unsafe extern "C" fn(key: LPCSTR, value: LPCSTR),
-    /// バイナリを取得
+    /// プロジェクトに保存されているバイナリデータを取得します
+    /// key		: キー名(UTF-8)
+    /// data		: 取得するデータの格納先へのポインタ
+    /// size		: 取得するデータのサイズ (保存されているサイズと異なる場合は失敗します)
+    /// 戻り値	: 正しく取得出来た場合はtrue
     pub get_param_binary: unsafe extern "C" fn(key: LPCSTR, data: *mut c_void, size: i32) -> bool,
-    /// バイナリを保存（サイズ上限 4096 バイト）
+    /// プロジェクトにバイナリデータを保存します
+    /// key		: キー名(UTF-8)
+    /// data		: 保存するデータへのポインタ
+    /// size		: 保存するデータのサイズ (4096バイト以下)
     pub set_param_binary: unsafe extern "C" fn(key: LPCSTR, data: *mut c_void, size: i32),
-    /// すべての保存データを削除
+    /// プロジェクトに保存されているデータを全て削除します
     pub clear_params: unsafe extern "C" fn(),
 }
 
 /// ホストアプリケーション構造体
 #[repr(C)]
 pub struct HOST_APP_TABLE {
-    /// プラグイン情報の設定
+    /// プラグインの情報を設定する
+    /// information	: プラグインの情報
     pub set_plugin_information: unsafe extern "C" fn(information: LPCWSTR),
 
-    /// 入力プラグインの登録
+    /// 入力プラグインを登録する
+    /// input_plugin_table	: 入力プラグイン構造体
     pub register_input_plugin: unsafe extern "C" fn(input_plugin_table: *mut INPUT_PLUGIN_TABLE),
-    /// 出力プラグインの登録
+    /// 出力プラグインを登録する
+    /// output_plugin_table	: 出力プラグイン構造体
     pub register_output_plugin: unsafe extern "C" fn(output_plugin_table: *mut OUTPUT_PLUGIN_TABLE),
-    /// フィルタプラグインの登録
+    /// フィルタプラグインを登録する
+    /// filter_plugin_table	: フィルタプラグイン構造体
     pub register_filter_plugin: unsafe extern "C" fn(filter_plugin_table: *mut FILTER_PLUGIN_TABLE),
-    /// スクリプトモジュールの登録
+    /// スクリプトモジュールを登録する
+    /// script_module_table	: スクリプトモジュール構造体
     pub register_script_module:
         unsafe extern "C" fn(script_module_table: *mut SCRIPT_MODULE_TABLE),
 
-    /// インポートメニューの登録
+    /// インポートメニューを登録する
+    /// name				: インポートメニューの名称
+    /// func_proc_import	: インポートメニュー選択時のコールバック関数
     pub register_import_menu:
         unsafe extern "C" fn(name: LPCWSTR, func_proc_import: unsafe extern "C" fn(*mut EDIT_SECTION)),
-    /// エクスポートメニューの登録
+    /// エクスポートメニューを登録する
+    /// name				: エクスポートメニューの名称
+    /// func_proc_export	: エクスポートメニュー選択時のコールバック関数
     pub register_export_menu:
         unsafe extern "C" fn(name: LPCWSTR, func_proc_export: unsafe extern "C" fn(*mut EDIT_SECTION)),
 
-    /// ウィンドウクライアントの登録
+    /// ウィンドウクライアントを登録する
+    /// name		: ウィンドウの名称
+    /// hwnd		: ウィンドウハンドル
+    /// ウィンドウにはWS_CHILDが追加され親ウィンドウが設定されます ※WS_POPUPは削除されます
     pub register_window_client: unsafe extern "C" fn(name: LPCWSTR, hwnd: HWND),
 
-    /// 編集ハンドルの作成
+    /// プロジェクトデータ編集用のハンドルを取得します
+    /// 戻り値	: 編集ハンドル
     pub create_edit_handle: unsafe extern "C" fn() -> *mut EDIT_HANDLE,
 
-    /// プロジェクトロード直後のハンドラ登録
+    /// プロジェクトファイルをロードした直後に呼ばれる関数を登録する ※プロジェクトの初期化時にも呼ばれます
+    /// func_project_load	: プロジェクトファイルのロード時のコールバック関数
     pub register_project_load_handler:
         unsafe extern "C" fn(func_project_load: unsafe extern "C" fn(*mut PROJECT_FILE)),
-    /// プロジェクトセーブ直前のハンドラ登録
+    /// プロジェクトファイルをセーブする直前に呼ばれる関数を登録する
+    /// func_project_save	: プロジェクトファイルのセーブ時のコールバック関数
     pub register_project_save_handler:
         unsafe extern "C" fn(func_project_save: unsafe extern "C" fn(*mut PROJECT_FILE)),
 }
-
