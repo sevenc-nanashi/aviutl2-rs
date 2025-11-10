3a196b66cdc5e168c43ddc5ac6870647d4d9dd13
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-11-10 17:29:54 +0900

    chore: table_converterを非公開に

--- crates/aviutl2/src/module/param.rs
+++ crates/aviutl2/src/module/param.rs
@@ -808,7 +808,7 @@ impl IntoScriptModuleReturnValue for std::collections::HashMap<String, String> {
 }
 
 #[doc(hidden)]
-pub mod table_converter {
+pub mod __table_converter {
     pub trait ToOptionalTableEntry {
         type Value;
         fn to_optional(&self) -> Option<Self::Value>;
