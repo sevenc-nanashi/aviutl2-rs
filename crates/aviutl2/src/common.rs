acba72222fb4103bc60bd796c0f1cd41d3be656a
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-02-01 14:41:15 +0900

    feat!: beta31を最低要件に

--- crates/aviutl2/src/common.rs
+++ crates/aviutl2/src/common.rs
@@ -13,7 +13,7 @@ pub struct AviUtl2Info {
 }
 
 /// 対応する最小のAviUtl2バージョン。
-pub const MINIMUM_AVIUTL2_VERSION: AviUtl2Version = AviUtl2Version(2003001);
+pub const MINIMUM_AVIUTL2_VERSION: AviUtl2Version = AviUtl2Version(2003100);
 
 /// AviUtl2のバージョンがサポート範囲かを確認します。
 pub fn ensure_minimum_aviutl2_version(version: AviUtl2Version) -> AnyResult<()> {
